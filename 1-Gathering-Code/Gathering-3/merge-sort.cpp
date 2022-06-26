197988bd9c74f7a41b14245eed2f60299fa8e2e5
williamk19 <williamkurniawan1144@gmail.com>
2022-06-26 11:35:51 +0700

    add code for merge and quick sort

--- 1-Gathering-Code/Gathering-3/merge-sort.cpp
+++ 1-Gathering-Code/Gathering-3/merge-sort.cpp
@@ -1,8 +1,58 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void merged(vector<int> &arr, int start, int end) {
+  int i = start;
+  int mid = (start + end) / 2;
+  int j = mid + 1;
+
+  vector<int> temp;
+  while(i <= mid and j <= end) {
+    if (arr[i] < arr[j]) {
+      temp.push_back(arr[i]);
+      i++;
+    } else {
+      temp.push_back(arr[j]);
+      j++;
+    }
+  }
+
+  while (i <= mid) {
+    temp.push_back(arr[i++]);
+  }
+
+  while (j <= end) {
+    temp.push_back(arr[j++]);
+  }
+
+  int k = 0;
+  for (int idx = start; idx <= end; idx++) {
+    arr[idx] = temp[k++];
+  }
+}
+
+void merge_sort(vector<int> &arr, int start, int end) {
+  if (start >= end) {
+    return;
+  }
+
+  int mid = (start + end) / 2;
+  merge_sort(arr, start, mid);
+  merge_sort(arr, mid + 1, end);
+  return merged(arr, start, end);
+}
 
 int main() {
+  vector<int> arr = { 10, 34, 2, 9, 1, 8, 3 };
+
+  int start = 0;
+  int end = arr.size() - 1;
+
+  merge_sort(arr, start, end);
+
+  for(int i=0; i<arr.size(); i++) {
+    cout << arr[i] << ", ";
+  }
 
   return 0;
 }
--- 1-Gathering-Code/Gathering-3/quick-sort.cpp
+++ 1-Gathering-Code/Gathering-3/quick-sort.cpp
@@ -1,7 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
+int partition(vector<int> &arr, int start, int end) {
+  int pivot = arr[end];
+  int i = start - 1;
 
+  for (int j = start; j < end; j++) {
+    if (arr[j] < pivot) {
+      i++;
+      swap(arr[i], arr[j]);
+    }
+  }
+  swap(arr[i + 1], arr[end]);
+  return i + 1;
+}
+
+void quick_sort(vector<int> &arr, int start, int end) {
+  if (start >= end) {
+    return;
+  }
+  int p = partition(arr, start, end);
+  quick_sort(arr, start, p-1);
+  quick_sort(arr, p+1, end);
+}
+
+int main() {
+  vector<int> arr = { 10, 34, 2, 9, 1, 8, 3 };
+  int start = 0;
+  int end = arr.size() - 1;
+  quick_sort(arr, start, end);
+
+  for (int i = 0; i < arr.size(); i++) {
+    cout << arr[i] << ", ";
+  }
   return 0;
 }
